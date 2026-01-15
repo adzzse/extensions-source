532be266aa43366642dfa2aa46a057a54cc1666d
Adzzse <noik0vshack@gmail.com>
2026-01-15 19:08:56 +0700

    add cuutruyen

---
+++ src/vi/cuutruyen/src/io/github/beerpsi/tachiyomi/extension/vi/cuutruyen/dto/SearchByTagDto.kt
@@ -0,0 +1,9 @@
+package eu.kanade.tachiyomi.extension.vi.cuutruyen.dto
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class SearchByTagDto(
+        val mangas: List<MangaDto>,
+        val tag: TagDto,
+)
